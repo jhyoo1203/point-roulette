368e89de411db0ae41dd1dccf43041d55e36f9f4
JaeHyeon Yoo <jhyoo1203@gmail.com>
2026-02-05 18:01:07 +0900

    feat: 룰렛 관련 에러코드 추가(#17)

--- backend/src/main/kotlin/com/pointroulette/presentation/exception/ErrorCode.kt
+++ backend/src/main/kotlin/com/pointroulette/presentation/exception/ErrorCode.kt
@@ -8,4 +8,8 @@ enum class ErrorCode(val httpStatus: HttpStatus, val message: String) {
     INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "입력값이 유효하지 않습니다."),
     BUSINESS_ERROR(HttpStatus.BAD_REQUEST, "비즈니스 로직 처리 중 오류가 발생했습니다."),
     RESOURCE_NOT_FOUND(HttpStatus.NOT_FOUND, "요청한 리소스가 DB에 존재하지 않습니다."),
+
+    // 룰렛
+    ALREADY_ROULETTE_PARTICIPATED(HttpStatus.BAD_REQUEST, "오늘 이미 룰렛에 참여했습니다."),
+    ROULETTE_PARTICIPATION_FAILED(HttpStatus.BAD_REQUEST, "룰렛 참여에 실패했습니다."),
 }
