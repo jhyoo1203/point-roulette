d158e96c030ba7ccd626cd44d761656e8d2c9402
JaeHyeon Yoo <jhyoo1203@gmail.com>
2026-02-06 00:08:36 +0900

    refactor: BusinessException 상속 받도록 수정(#21)

--- backend/src/main/kotlin/com/pointroulette/domain/point/InsufficientPointException.kt
+++ backend/src/main/kotlin/com/pointroulette/domain/point/InsufficientPointException.kt
@@ -1,5 +1,8 @@
 package com.pointroulette.domain.point
 
+import com.pointroulette.presentation.exception.BusinessException
+import com.pointroulette.presentation.exception.ErrorCode
+
 /**
  * 포인트 부족 예외
  * - 포인트 사용 시 잔액이 부족한 경우 발생
@@ -10,6 +13,7 @@ package com.pointroulette.domain.point
 class InsufficientPointException(
     val currentPoint: Int,
     val requestedAmount: Int
-) : RuntimeException(
+) : BusinessException(
+    ErrorCode.INSUFFICIENT_POINT,
     "포인트가 부족합니다. 현재 포인트: $currentPoint, 필요 포인트: ${-requestedAmount}"
 )
